d9272439be1315c861cba11af405c195c19693d9
rabzkhan <rabbzkhan14@gmail.com>
2020-05-19 21:38:39 +0600

    ---

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text('I AM RICH')),
+            backgroundColor: Colors.indigoAccent,
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/diamond.png'),
+            ),
+          ),
+        ),
+      ),
+    );
